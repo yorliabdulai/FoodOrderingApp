428e3a6350b9ca5f18312a8a52a124537a0cc1eb
yorliabdulai <yorliabdulai1@gmail.com>
2024-03-06 20:11:08 +0000

    Added a function to listen to app start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,5 +7,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/test",async (req: Request, res: Response) => {
-    
+    res.json({message: "Hello World!"});
+});
+app.listen(7000, () => {
+    console.log("Server is running on localhost:7000");
 });
