e64057cbc99e0f06f2a25d6b1231de418aae6776
yorliabdulai <yorliabdulai1@gmail.com>
2024-03-09 19:12:31 +0000

    Configure stles to use global.css and deleted app.css, index.css and app.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
+import './global.css'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import AppRoutes from './AppRoutes'
