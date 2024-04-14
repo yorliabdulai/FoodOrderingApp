78f2550a05771dbdfa4c38cbfa91e8d049e54c8e
yorliabdulai <yorliabdulai1@gmail.com>
2024-04-14 19:31:12 +0000

    Add MobileNav component to Header

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,24 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const MobileNav = () => {
+  return(
+    <Sheet>
+       <SheetTrigger>
+         <Menu className="text-orange-500"/>
+       </SheetTrigger> 
+       <SheetContent>
+        <SheetTitle>
+            <span>Welcome to CampusChop.com!</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+       </SheetContent>
+    </Sheet>
+  )
+}
+export default MobileNav;
