36cf9b8c63a0d3489da8e0d2e33e13ab277e7a78
yorliabdulai <yorliabdulai1@gmail.com>
2024-04-15 20:04:36 +0000

    Created MainNav component

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+   return(
+    <Button variant="ghost" className="font-bold hover:text-orage-500 hover:bg-white">
+     Log In
+    </Button>
+   )
+}
+export default MainNav;
