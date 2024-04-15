35225fc0d950e4993d3f20d40e7327b0e0b600d0
yorliabdulai <yorliabdulai1@gmail.com>
2024-04-15 20:07:37 +0000

    Add MainNav component to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -9,6 +10,9 @@ const Header = () => {
             </Link>
             <div className="md:hidden">
               <MobileNav />
+            </div>
+            <div className="hidden md:block">
+              <MainNav />
             </div>
         </div>
     </div>
