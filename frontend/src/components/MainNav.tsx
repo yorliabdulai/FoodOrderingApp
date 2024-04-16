1f01aa213c50ce2c78e23cb659b16a791739b7d8
yorliabdulai <yorliabdulai1@gmail.com>
2024-04-16 18:35:01 +0000

    Fix typo in MainNav component

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -2,7 +2,7 @@ import { Button } from "./ui/button";
 
 const MainNav = () => {
    return(
-    <Button variant="ghost" className="font-bold hover:text-orage-500 hover:bg-white">
+    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
      Log In
     </Button>
    )
