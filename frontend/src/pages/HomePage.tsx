e7751b8ef186e2e763f29bd4dd4c35e51bc5f2ab
yorliabdulai <yorliabdulai1@gmail.com>
2024-04-17 18:43:00 +0000

    Add landing image to homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
+import landingImage from "../assets/landing.png"
 const HomePage = () => {
     return (
         <div className="flex flex-col gap-12">
           <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
             <h1 className="text-5xl font-bold tracking-tight text-orange-600">Tuck into a takeaway today</h1>
             <span className="text-xl">Food is just a click away!</span>
+          </div>
+          <div className="grid md:grid-cols-2 gap-5">
+            <img src={landingImage} />
+            
           </div>
         </div>
     );
