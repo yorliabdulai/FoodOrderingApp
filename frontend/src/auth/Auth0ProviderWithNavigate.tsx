0b44da4df88d45e3283a89d9d056eaa5d4968286
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-05 19:32:21 +0000

    Refactor Auth0ProviderWithNavigate to include environment variables

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,8 +1,12 @@
 type Props = {
-
+ children: React.ReactNode;
 }
 
-const Auth0ProviderWithNavigate = ({}:Props) => {
+const Auth0ProviderWithNavigate = ({children}:Props) => {
+ const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+ const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+ const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
 
 }
 
