1614a7e2ab7c9f6e3a88c7e49c760e489ebab5df
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-08 14:50:04 +0000

    Add user profile link to UsernameMenu component

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -1,6 +1,7 @@
 import { CircleUserRound } from "lucide-react";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const UsernameMenu = () => {
     const {user} = useAuth0();
@@ -10,6 +11,13 @@ const UsernameMenu = () => {
               <CircleUserRound className="text-orange-500" />
               {user?.email}
             </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                <Link to="/user-profile" 
+                className="font-bold hover:text-orange-500">User Profile</Link>
+                </DropdownMenuItem> 
+            
+            </DropdownMenuContent>
         </DropdownMenu>
     )
 };
