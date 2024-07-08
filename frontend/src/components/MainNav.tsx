393acda969c5c141c887257348f24ac64d826e71
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-08 14:58:39 +0000

    Add UsernameMenu component to MainNav

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
    const {loginWithRedirect, isAuthenticated} = useAuth0();
