696b6d254da4ed33f461cce5f6fc74fe1c6d46b3
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-08 14:56:13 +0000

    Add Separator and Log Out button to UsernameMenu component

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -2,6 +2,8 @@ import { CircleUserRound } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
     const {user} = useAuth0();
@@ -16,7 +18,10 @@ const UsernameMenu = () => {
                 <Link to="/user-profile" 
                 className="font-bold hover:text-orange-500">User Profile</Link>
                 </DropdownMenuItem> 
-            
+                <Separator />
+                <DropdownMenuItem>
+                  <Button className="flex flex-1 font-bold bg-orange-500">Log Out</Button>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
