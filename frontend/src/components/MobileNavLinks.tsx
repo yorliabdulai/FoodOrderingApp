a8d58b3928f8dac23dbb4763885d8324da180d0c
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-09 15:41:15 +0000

    Add Button component to MobileNavLinks

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const MobileNavLinks = () => {
     return(
@@ -6,6 +7,9 @@ const MobileNavLinks = () => {
       <Link to="/user-profile"className="flex items-center bg-white font-bold hover:text-orange-500">
         User Profile
       </Link>
+      <Button className="flex items-center font-bold px-3 hover:bg-gray-500">
+        Log Out
+      </Button>
       </>  
     )
 }
