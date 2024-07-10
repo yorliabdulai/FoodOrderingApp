d757c054e8741c52600ba518a40ae002af45963c
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-10 15:24:56 +0000

    Add Auth0 integration to MobileNavLinks component

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MobileNavLinks = () => {
     const {logout} = useAuth0();
