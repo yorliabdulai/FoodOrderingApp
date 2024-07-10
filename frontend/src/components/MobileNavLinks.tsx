d7ea5c78d5f8a53901bf22ffd0a92f1e897dfc87
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-10 15:24:31 +0000

    Add logout functionality to MobileNavLinks component

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
 const MobileNavLinks = () => {
+    const {logout} = useAuth0();
     return(
       <>
       <Link to="/user-profile"className="flex items-center bg-white font-bold hover:text-orange-500">
         User Profile
       </Link>
-      <Button className="flex items-center font-bold px-3 hover:bg-gray-500">
+      <Button onClick={() => logout()} className="flex items-center font-bold px-3 hover:bg-gray-500">
         Log Out
       </Button>
       </>  
